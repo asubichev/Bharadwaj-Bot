ce7b2fd64fe609c292013ce064aa249d7f82719f
krishnanyash <yashkrishnan@outlook.com>
2023-06-28 19:19:38 -0500

    token added

--- main/index.js
+++ main/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const CLIENT_TOKEN="63a5f91f8f1d6b9d39c042c004c3ed635d1084198d23931c87df7de90b0ff235"
+const CLIENT_TOKEN="MTEyMzc2MjUzMjA3OTg5NDYyOA.GfKmqw.tCpH__bmqKV2xFT8bT0HRCP1s3OplToREbhmy4"
 
 const client = new Discord.Client(); //create new client
 
